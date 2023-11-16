94b4cd3a3bb7905cb4285b5c3a2d56691649c26b
romeouni <rahul.ulman@gmail.com>
2023-11-16 15:38:31 -0500

    gitIgnore + data splitting

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,24 +1,24 @@
 import numpy as np
 import pandas as pd
+import os
 
 
 
 def main():
+
     appearances = pd.read_csv("data/appearances.csv")
 
-    PL = appearances.where('leagueID == 1')
-    SA = appearances.where('leagueID == 2')
-    BL = appearances.where('leagueID == 3')
-    LL = appearances.where('leagueID == 4')
-    LU = appearances.where('leagueID == 5')
+    PL = appearances.query('leagueID == 1')
+    SA = appearances.query('leagueID == 2')
+    BL = appearances.query('leagueID == 3')
+    LL = appearances.query('leagueID == 4')
+    LU = appearances.query('leagueID == 5')
 
-    PL.to_csv("/data")
-    SA.to_csv("/data")
-    BL.to_csv("/data")
-    LL.to_csv("/data")
-    LU.to_csv("/data")
-    
-    return None
+    PL.to_csv('data/england.csv', index=False)
+    SA.to_csv('data/italy.csv', index=False)
+    BL.to_csv('data/germany.csv', index=False)
+    LL.to_csv('data/spain.csv', index=False)
+    LU.to_csv('data/france.csv', index=False)
 
 if __name__ == "__main__":
     main()
