9b3a19ada18428c1afba9c4246b2ca4b8fe0b21a
romeouni <rahul.ulman@gmail.com>
2023-11-28 10:37:39 -0500

    PCA update

--- data_cleanup.py
+++ data_cleanup.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import os
 
 
 
@@ -15,6 +16,33 @@ def main():
     yTotal = appearances[["positionOrder", "leagueID"]]
 
     appearancesFeatures = appearances.drop(nonFeatures, axis=1)
+
+    #recode positions to reduce number of categories
+
+    # KEY:
+    # 1: Goalkeepers
+    # 2: Fullbacks
+    # 3: Centerbacks
+    # 4: Defensive Midfielders
+    # 5: Midfielders
+    # 6: Attacking Midfielders
+    # 7: Wingers
+    # 8: Strikers
+    
+    yTotal = yTotal.replace(4, 2)
+    yTotal = yTotal.replace(5|6|7, 4)
+    yTotal = yTotal.replace(8|9|10, 5)
+    yTotal = yTotal.replace(11|12|13, 6)
+    yTotal = yTotal.replace(14|15, 7)
+    yTotal = yTotal.replace(16, 8)
+
+    print(yTotal['positionOrder']==17)
+
+    subs = yTotal.index[yTotal['positionOrder'] == 17].tolist()
+
+    appearancesFeatures = appearancesFeatures.drop(index=subs)
+    yTotal = yTotal.drop(index=subs)
+
 
     # Perform standard scaling on the features
     scaler = StandardScaler()
@@ -31,32 +59,33 @@ def main():
     # Visualize the loading values for each principal component separately
     num_components = loading_values.shape[1]
 
-    # for i in range(num_components):
-    #     sorted_indices = np.argsort(loading_values[:, i])
-    #     plt.figure(figsize=(10, 6))
-    #     plt.barh(range(len(loading_values)), loading_values[sorted_indices, i], align='center')
-    #     plt.yticks(range(len(loading_values)), appearancesFeatures.columns[sorted_indices])
-    #     plt.xlabel('Loading Value')
-    #     plt.title(f'Principal Component {i+1} Loadings')
-    #     plt.tight_layout()
-    #     plt.show()
+    for i in range(num_components):
+        sorted_indices = np.argsort(loading_values[:, i])
+        plt.figure(figsize=(10, 6))
+        plt.barh(range(len(loading_values)), loading_values[sorted_indices, i], align='center')
+        plt.yticks(range(len(loading_values)), appearancesFeatures.columns[sorted_indices])
+        plt.xlabel('Loading Value')
+        plt.title(f'Principal Component {i+1} Loadings')
+        plt.tight_layout()
+        plt.show()
 
-    # # Get explained variance ratios for each principal component
-    # explained_var_ratio = pca.explained_variance_ratio_
+    # Get explained variance ratios for each principal component
+    explained_var_ratio = pca.explained_variance_ratio_
 
-    # # Create a bar chart of explained variance for each component
-    # num_components = len(explained_var_ratio)
-    # component_index = range(1, num_components + 1)
+    # Create a bar chart of explained variance for each component
+    num_components = len(explained_var_ratio)
+    component_index = range(1, num_components + 1)
 
-    # plt.figure(figsize=(10, 6))
-    # plt.bar(component_index, explained_var_ratio * 100, align='center', alpha=0.7)
-    # plt.xlabel('Principal Component')
-    # plt.ylabel('Explained Variance (%)')
-    # plt.title('Explained Variance of Principal Components')
-    # plt.xticks(component_index)
-    # plt.grid(axis='y')
-    # plt.show()
+    plt.figure(figsize=(10, 6))
+    plt.bar(component_index, explained_var_ratio * 100, align='center', alpha=0.7)
+    plt.xlabel('Principal Component')
+    plt.ylabel('Explained Variance (%)')
+    plt.title('Explained Variance of Principal Components')
+    plt.xticks(component_index)
+    plt.grid(axis='y')
+    plt.show()
 
+    appearances = np.delete(appearances, subs, axis=0)
     xPL = appearances.query('leagueID == 1').drop(nonFeatures, axis=1)
     xSA = appearances.query('leagueID == 2').drop(nonFeatures, axis=1)
     xBL = appearances.query('leagueID == 3').drop(nonFeatures, axis=1)
---
+++ gb_trees/gb_hyperparameter.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import accuracy_score
+
+def main():
+    X = pd.read_csv("data/xWhole.csv").to_numpy()
+    y = pd.read_csv("data/yWhole.csv").to_numpy()
+
+    subs = np.where(y==17)[0]
+
+    X = np.delete(X, subs, axis=0)
+    y = np.delete(y, subs, axis=0)
+
+    n, d = X.shape
+    permutation = np.random.permutation(n)
+
+    X = X[permutation]
+    y = y[permutation].ravel()
+
+
+    xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.2, random_state=4)
+    gbc = HistGradientBoostingClassifier(max_iter=300, max_leaf_nodes=100, learning_rate=0.01)
+
+    # paramGrid = {
+    #     "max_iter": [3, 10, 30, 100, 300, 1000],
+    #     "max_leaf_nodes": [2, 5, 10, 20, 50, 100],
+    #     "learning_rate": np.arange(0.01, 0.1, 0.05)
+    #     # In this model, other hyperparameters are coupled to these three, so no need to optimize them.
+    # }
+
+    # gridSearch = GridSearchCV(estimator=gbc, param_grid=paramGrid, cv=5, scoring='accuracy')
+    # gridResult = gridSearch.fit(xTrain, yTrain)
+
+    # print("Best parameters:", gridResult.best_params_)
+
+    # optimalModel = gridResult.best_estimator_
+    # accuracy = optimalModel.score(xTest, yTest)
+    # print("Accuracy of optimal model on test set:", accuracy)
+
+    # Best parameters: {'learning_rate': 0.01, 'max_iter': 300, 'max_leaf_nodes': 100}
+    # Accuracy of optimal model on test set: 0.4777218349859052   
+
+    gbc.fit(xTrain, yTrain)
+    yHat = gbc.predict(xTrain)
+
+    accuracy = accuracy_score(yTest, yHat)
+
+    print(accuracy)
+
+
+if __name__ == "__main__":
+    main()
